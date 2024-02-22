60cc87913c6b58005eb94883e809e84dd8e2e671
Joselito Machado <opaguelosantos@gmail.com>
2024-02-22 01:58:37 -0300

    feat: prop dos medicamentos, e metedo de receber a quantidade do medicamento

---
+++ src/ControleEstoque/Models/Medicamento.cs
@@ -0,0 +1,18 @@
+﻿namespace ControleEstoque.Models
+{
+    public class Medicamento
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string PricipioAtivo { get; set; } = string.Empty;
+        public string Fabricante { get; set; } = string.Empty;
+        public string CodigoBarra { get; set; } = string.Empty;
+        public string CodigoInterno { get; set; } = string.Empty;
+        public int Quantidade { get;  private set; }
+
+        public bool Receber(Medicamento codigoMedicamento, int quantidade)
+        {
+            codigoMedicamento.Quantidade += quantidade;
+            return true;
+        }
+    }
+}
